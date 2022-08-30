8f1c03fadd6d9f53e66a5a175e0054d46c642f0e
Ilya <kucerubov365@gmail.com>
2022-08-30 11:58:05 +0300

    hm

--- src/index.js
+++ src/index.js
@@ -7,7 +7,21 @@
  * @param {number} maxSecond
  * @returns {number}
  */
-export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
+export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {
+  let min;
+  let max;
+  if(minFirst > minSecond){
+    min = minSecond;
+  }else {
+    min = minFirst;
+  }
+  if (maxFirst > maxSecond){
+    max = maxFirst;
+  }else {
+    max = maxSecond;
+  }
+  return min*max;
+};
 
 /**
  * Функция принимает один числовой аргумент, это радиус круга. Функция должна
@@ -16,7 +30,9 @@ export const minMaxMultiply = (minFirst, minSecond, maxFirst, maxSecond) => {};
  * @param {number} radius
  * @returns {number}
  */
-export const getSquareOfCircle = (radius) => {};
+export const getSquareOfCircle = (radius) => {
+  return Math.PI *  Math.pow(radius,2);
+};
 
 /**
  * Функция принимает число и точность к которой нужно округлить
@@ -26,7 +42,10 @@ export const getSquareOfCircle = (radius) => {};
  * @param {number} accuracy
  * @returns {number}
  */
-export const getFixedValue = (number, accuracy) => {};
+export const getFixedValue = (number, accuracy) => {
+    let rounding = Math.pow(10, accuracy);
+    return Math.round(number * rounding) / rounding;
+};
 
 /**
  * Если целая часть числа четная, то срабатывает Math.floor для этого числа
@@ -34,7 +53,13 @@ export const getFixedValue = (number, accuracy) => {};
  * @param {number} number
  * @returns {number}
  */
-export const getFlooroCeil = (number) => {};
+export const getFlooroCeil = (number) => {
+  if(Math.trunc(number) % 2 === 0){
+      return Math.floor(number)
+  }else {
+    return Math.ceil(number);
+  }
+};
 
 /**
  * Мы передаем два числа, от какого и до какого числа и функция должна
@@ -44,4 +69,11 @@ export const getFlooroCeil = (number) => {};
  * @param {number} to
  * @returns {number}
  */
-export const randomizer = (from, to) => {};
+export const randomizer = (from, to) => {
+  let rand = Math.ceil(Math.random() * to);
+  if (rand < from){
+    return from;
+  }else {
+    return rand;
+  }
+};
